51bea9a6bf0b4441497effe02afdfc54625b3561
Hugh Rawlinson <hugh@spotify.com>
2016-03-02 19:54:03 +0100

    Zero Crossing Rate

--- app/Main.hs
+++ app/Main.hs
@@ -9,3 +9,8 @@ main = do
   print (equal "" "")
   print (not (equal [1,2,3] [2,3,4]))
   print (not (equal "Test" "Tests"))
+  print (zeroCrossings [-1,1,-1])
+  print (zeroCrossings [-1,1,1,1,-1,-1])
+  print (zeroCrossings [-1,1,-1,1,-1,1,-1])
+  print (zeroCrossings [1,1,1,1,1,1,1,1,1,1])
+  print (zeroCrossings [-1,1,-1])
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,5 +1,6 @@
 module Lib
-    ( equal
+    ( equal,
+      zeroCrossings
     ) where
 
 equal :: (Eq a) => [a] -> [a] -> Bool
@@ -7,3 +8,12 @@ equal [] [] = True
 equal (x:xs) (y:ys)
   | x == y      = equal xs ys
 equal _ _ = False
+
+zeroCrossings :: (Ord a, Num a, Integral b) => [a] -> b
+zeroCrossings xs = zc xs 0 where
+  zc :: (Ord a, Num a, Integral b) => [a] -> b -> b
+  zc (x:y:xs) count
+    | x > 0 && y < 0 = zc (y:xs) count + 1
+    | x < 0 && y > 0 = zc (y:xs) count + 1
+    | otherwise = zc (y:xs) count
+  zc [x] count = count
