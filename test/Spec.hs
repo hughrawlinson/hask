814918912ab786bf28e44b9b98ea04cedddb03ee
Hugh Rawlinson <hugh@codeoclock.net>
2016-03-27 16:31:19 +0200

    yay

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,13 +1,1 @@
-import Lib
-import Test.HUnit
-
-zeroCrossingTests = TestList [
-    TestCase $
-      assertEqual "One Zero Crossing" (zeroCrossings [1, -1]) 1
-  ]
-
-main :: IO Counts
-main = 
-  runTestTT zeroCrossingTests
---	runTestTT zeroCrossingRateTests
-
+{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
