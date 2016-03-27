977ad356361470ee3fab1c144dbb64ebaa885f53
Hugh Rawlinson <hugh@codeoclock.net>
2016-03-27 03:00:36 +0200

    tests?

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,15 +1,9 @@
 import Lib
-import Test.Hspec
-import Test.QuickCheck
+import Test.HUnit
+
+test1 = TestCase (assertEqual "One Zero Crossing" (zeroCrossings [1,-1]) 1)
 
 main :: IO ()
-main = hspec $ do
-  describe "hask.equal.head" $ do
-    it "correctly identifies equal strings" $ do
-      equal "Test" "Test" `shouldBe` True
+main = do
+  putStrLn runTestTT test1
 
-    it "returns the first element of an *arbitrary* list" $
-      property $ \x xs -> head (x:xs) == (x :: Int)
-
-    it "throws an exception if used with an empty list" $ do
-      evaluate (head []) `shouldThrow` anyException
