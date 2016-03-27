61eb08a21a1d18d066c0bb93dd0205eba2651422
Hugh Rawlinson <hugh@codeoclock.net>
2016-03-27 15:01:56 +0200

    I'll figure out how to have multiple tests, later

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,18 +1,13 @@
 import Lib
 import Test.HUnit
 
-test1 :: Test
-test1 = TestCase (assertEqual "One Zero Crossing" (zeroCrossings [1,-1]) 1)
-
-tests = TestList [
-    TestLabel "lkajsOne Zero Crossing" TestCase (
-      assertEqual "One Zero Crossing" (zeroCrossings [1,-1]) 1
-    ),
-		TestLabel "Many Zero Crossings" TestCase (
-      assertEqual "Seven Zero Crossings" (zeroCrossings (take 8 $ cycle [1,-1])) 7
-    )
+zeroCrossingTests = TestList [
+    TestCase $
+		  assertEqual "One Zero Crossing" (zeroCrossings [1, -1]) 1
   ]
 
 main :: IO Counts
-main = runTestTT tests
+main = 
+  runTestTT zeroCrossingTests
+--	runTestTT zeroCrossingRateTests
 
