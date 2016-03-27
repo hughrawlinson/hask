ce4865b5efb9ec52f9c1a1e04be213d759462cac
Hugh Rawlinson <hugh@codeoclock.net>
2016-03-27 14:39:34 +0200

    nearly wurks

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,9 +1,18 @@
 import Lib
 import Test.HUnit
 
+test1 :: Test
 test1 = TestCase (assertEqual "One Zero Crossing" (zeroCrossings [1,-1]) 1)
 
 tests = TestList [
-		TestLabel "One Zero Crossing" test1
-	]
+    TestLabel "lkajsOne Zero Crossing" TestCase (
+      assertEqual "One Zero Crossing" (zeroCrossings [1,-1]) 1
+    ),
+		TestLabel "Many Zero Crossings" TestCase (
+      assertEqual "Seven Zero Crossings" (zeroCrossings (take 8 $ cycle [1,-1])) 7
+    )
+  ]
+
+main :: IO Counts
+main = runTestTT tests
 
